87266141cfd2f95c83e55ae0717ebce4f0228eb8
Nathan Glimsdale <nglimsdale@gmail.com>
2016-01-25 09:42:32 -0600

    refactor: refactored led example
    moved led operations into functions. moved initialization into a
    function.

--- week1/main.c
+++ week1/main.c
@@ -1,16 +1,50 @@
 #define F_CPU 16000000
+#include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
+uint8_t yellow_led = PORTC7;
+uint8_t green_led = PORTD5;
+
+void init();
+void set_green_led(uint8_t value);
+void set_yellow_led(uint8_t value);
+
 int main()
 {
-    DDRC |= (1 << DDC7);// Make pin 13 be an output.  
+    init();
     while(1)
     {
-        PORTC |= (1 << PORTC7);// Turn the LED on.
-        _delay_ms(50);
-        PORTC &= ~(1 << PORTC7);// Turn the LED off.
+        set_yellow_led(1);
+        set_green_led(1);
+        _delay_ms(5000);
+        set_yellow_led(0);
+        set_green_led(0);
         _delay_ms(5000);
     }
 }
 
+void init()
+{
+    /* Set the Green and Yellow LED Pins to outputs */
+    DDRC |= (1 << DDC7);
+    DDRD |= (1 << DDD5);
+}
+
+void set_yellow_led(uint8_t value)
+{
+    /* The pin for the yellow LED needs to be driven high to turn it on */
+    if (value == 1)
+        PORTC |= (1 << yellow_led);
+    else if (value == 0)
+        PORTC &= ~(1 << yellow_led); 
+}
+
+void set_green_led(uint8_t value)
+{
+    /* The pin for the green LED needs to be driven low to turn it on */
+    if (value == 1)
+        PORTD &= ~(1 << green_led);
+    else if (value == 0)
+        PORTD |= (1 << green_led);
+}
