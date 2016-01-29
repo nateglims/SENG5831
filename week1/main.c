fe9d899f0594ddb913ba85430d8db78ec12272e9
Nathan Glimsdale <nglimsdale@gmail.com>
2016-01-28 22:59:46 -0600

    (WIP) still debugging debounce...

--- week1/main.c
+++ week1/main.c
@@ -12,24 +12,43 @@ void init();
 void set_green_led(uint8_t value);
 void set_yellow_led(uint8_t value);
 
+enum BUTTON {
+    BUTTON_C,
+    BUTTON_B,
+    BUTTON_A,
+} button;
 
 int main()
 {
     init();
     while(1)
     {
-        while ((PINB & 0b00001001) != 0);
-        set_green_led(1);
-        _delay_ms(10);
-        set_green_led(0);
-
-        while((PINB & 0b00001001) == 0);
+        button = BUTTON_B;
+        while ((PINB & 0x09) == 0x09);
         _delay_ms(10);
 
-        set_yellow_led(1);
-        _delay_ms(1000);
-        set_yellow_led(0);
+        while((PINB & 0x08) || (PINB & 0x01))
+        {
+            if ((PINB & 0x08) == 0)
+                button = BUTTON_A;
+            else if ((PINB & 0x01) == 0)
+                button = BUTTON_C;
+            else
+                set_green_led(1);
+        };
+        _delay_ms(10);
 
+        if (button == BUTTON_A) {
+            set_yellow_led(1);
+            _delay_ms(1000);
+            set_yellow_led(0);
+        } else if (button == BUTTON_C) {
+            set_green_led(1);
+            _delay_ms(1000);
+            set_green_led(0);
+        } else {
+            set_yellow_led(1);
+        }
     }
 }
 
