eb312cefb5c925dc9e5dc7d96b5d8a575740df5c
Nathan Glimsdale <nglimsdale@gmail.com>
2016-02-06 15:45:08 -0600

    timer: ctc timer demo
    blink two LEDs, one in a loop with delays and one in an interrupt with a
    timer compare.

--- timer/timer.c
+++ timer/timer.c
@@ -1,26 +1,37 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define F_CPU 16000000
+#include <util/delay.h>
+
+ISR(TIMER1_COMPA_vect)
+{
+   PORTC ^= (1 << PORTC7); 
+}
 
 void init()
 {
     DDRC |= (1 << DDC7);
     DDRD |= (1 << DDD5);
-    TCCR1B = (1 << CS10) | (1 << CS12);
-    TCNT1 = 0x00;
+    TCCR1A = 0x00;
+    TCCR1B = (1 << CS10) | (1 << CS12) | (1 << WGM12);
+    OCR1A = 15624;
+    TCNT1 = 0x0000;
+    TIMSK1 = (1 << OCIE1A);
+    USBCON = 0x00;
+    sei();
 }
 
 int main()
 {
     init();
-    PORTC &= ~(1 << PORTC7);
     PORTD &= ~(1 << PORTD5);
+
+    PORTC &= ~(1 << PORTC7);
     while (1)
     {
-        if (TCNT1 > 15625)
-        {
-            PORTC ^= (1 << PORTC7);
-            PORTD ^= (1 << PORTD5);
-            TCNT1 = 0;
-        }
+        PORTD ^= (1 << PORTD5);
+        _delay_ms(1000);
     }
 }
 
